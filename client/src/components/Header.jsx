0a84b425b3a444e485bd52688676c44fdfafcc7d
Hristo <hristo.chukurov@gmail.com>
2025-03-31 22:50:41 +0300

    client side

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+
+    return (
+        <>
+            <header className="header">
+                <ul>
+                    <li className="link">Blog</li>
+                    <li className="link">Information</li>
+                    <li className="link">Location</li>
+                    <li className="link">Register</li>
+                </ul>
+            </header>
+        </>
+    )
+  }
+  
