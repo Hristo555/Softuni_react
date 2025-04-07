8f1980d70e38e2171a370c5fa22db8e1b524f61e
Hristo <hristo.chukurov@gmail.com>
2025-04-07 23:45:37 +0300

    dynamic nav

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router";
 import styles from "./Header.module.css"
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
 
 export default function Header(){
+    const {email} = useContext(UserContext);
     return(
         <header>
             <nav className={styles.main_nav}>
                 <Link className={styles.links} to="/">Home</Link>
-
                 <Link className={styles.links} to="/blogs">Blogs</Link>
-
-                <Link className={styles.links} to="/login">Login</Link>
-                <Link className={styles.links} to="/register">Register</Link>
-                <Link className={styles.links} to="/logout">Logout</Link>
+                {email ? (
+                    <div className="logged-wrapper">
+                        <Link className={styles.links} to="/logout">Logout</Link>
+                        {email}
+                    </div>
+                )
+                : (
+                    <div className="not-logged">
+                        <Link className={styles.links} to="/login">Login</Link>
+                        <Link className={styles.links} to="/register">Register</Link>
+                    </div>
+                )}
             </nav>
         </header>
     );
